b953be075aee05d3da56fd57f968608dc9099d3f
Marcos Piemontez <marcos.piemontez1@gmail.com>
2023-02-21 20:41:22 -0400

    feta: utilizando statefullwidget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,24 +1,56 @@
 import 'package:flutter/material.dart';
 import 'home_controller.dart';
 
-class HomePage extends StatelessWidget {
+// Usando stateless
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var controller = HomeController.of(context);
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Home'),
+//       ),
+//       body: Center(
+//         child: Text('Aprendendo Flutter ${controller.value}'),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: const Icon(Icons.add),
+//         onPressed: () {
+//           controller.increment();
+//         },
+//       ),
+//     );
+//   }
+// }
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    var controller = HomeController.of(context);
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  var counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
       ),
       body: Center(
-        child: Text('Aprendendo Flutter ${controller.value}'),
+        child: Text('Aprendendo Flutter $counter'),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          controller.increment();
+          setState(() {
+            counter++;
+          });
         },
       ),
     );
